cc4198485d8e5632e13240428316e22529b45fab
ahmed walid <ahmedwalid98.aw@gmail.com>
2023-07-12 01:45:27 +0300

    adding the add user functionality

--- app/src/main/java/com/example/monqez/ui/AddUserFragment.kt
+++ app/src/main/java/com/example/monqez/ui/AddUserFragment.kt
@@ -1,7 +1,11 @@
 package com.example.monqez.ui
 
+import android.app.Activity
 import android.app.ProgressDialog
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -32,9 +36,25 @@ class AddUserFragment : Fragment() {
                 Toast.makeText(requireContext(),"not validate", Toast.LENGTH_SHORT).show()
             }
         }
+        binding.profileImage.setOnClickListener {
+            val cInt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cInt, Image_Capture_Code)
+        }
         return binding.root
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == Activity.RESULT_OK) {
+                val bp = data?.extras?.get("data") as Bitmap
+                binding.profileImage.setImageBitmap(bp)
 
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(requireContext(), "Cancelled", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+    }
     private fun validate() : Boolean {
         var isValidate = true;
         if (binding.name.text!!.isEmpty()) {
